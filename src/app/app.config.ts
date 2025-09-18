a1e59b9ccafa9a90779f9f62a9ea6879792803cb
Roberto Guerrero <rjguerreroh@gmail.com>
2025-09-18 13:36:55 -0500

    add changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // HashStretegy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
   ]
 };
